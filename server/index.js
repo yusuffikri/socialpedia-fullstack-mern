ba0d42f9989565b1b85465e53f2650641aa59098
yusuffikri <yusuffiks99@gmail.com>
2023-02-04 20:21:55 +0800

    moongose setup

--- server/index.js
+++ server/index.js
@@ -35,3 +35,13 @@ const storage = multer.diskStorage({
 });
 
 const upload = multer({storage});
+
+
+// Mongoose Setup
+const PORT = process.env.PORT || 6001;
+mongoose.connect(process.env.MONGO_URL, {
+    useNewUrlParse: true,
+    useUnifiedTopology: true,
+}).then(() => {
+    app.listen(PORT, () => console.log(`Server Port: ${PORT}`))
+})
