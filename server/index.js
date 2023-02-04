74f5c874db7ec611b41076c28e792964ec1b5941
yusuffikri <yusuffiks99@gmail.com>
2023-02-04 20:41:20 +0800

    update mongoose setup

--- server/index.js
+++ server/index.js
@@ -40,8 +40,8 @@ const upload = multer({storage});
 // Mongoose Setup
 const PORT = process.env.PORT || 6001;
 mongoose.connect(process.env.MONGO_URL, {
-    useNewUrlParse: true,
+    useNewUrlParser: true,
     useUnifiedTopology: true,
 }).then(() => {
     app.listen(PORT, () => console.log(`Server Port: ${PORT}`))
-})
+}).catch((error)  => console.log(`${error} did not connect`))
